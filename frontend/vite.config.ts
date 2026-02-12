0b747a7aea2220d63809a9a5092d347e99c44e0c
Abdulrehman Shaikh <abdulrehman.shaikh29042@gmail.com>
2026-02-12 22:53:27 +0500

    UPDATED

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:8000',
-//         changeOrigin: true
-//       }
-//     }
-//   },
-//   build: {
-//     outDir: '../public',
-//     emptyOutDir: true
-//   }
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
